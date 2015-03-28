df87b4aed903728e03a8b6b4b3cb47b66cd8121d
Gaye Bulut <gayegul@gmail.com>
2015-03-28 01:54:28 -0700

    add solution

---
+++ powersOfTwo.js
@@ -0,0 +1,28 @@
+//For this challenge you will be determining 
+//whether or not a number is a power of two.
+
+function PowersofTwo(num) { 
+
+  for(var i = 0; i <= num; i++) {
+    if(Math.pow(2, i) === num) {
+      return true;
+    }
+  }
+  return false; 
+         
+};
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
