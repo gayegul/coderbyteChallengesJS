b4f9768cfd283d8c71558af147d75d8c4565f315
Gaye Bulut <gayegul@gmail.com>
2015-03-31 21:54:59 -0700

    easy

--- additivePersistence.js
+++ additivePersistence.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//For this challenge you will be determining 
+//the additive persistence for a given number.
+//29 -> 2+9=11, 11 -> 1+1=2, answer = 2 times
+
 function AdditivePersistence(num) { 
   var newArr = num.toString().split(""); 
   var counter = 0;
