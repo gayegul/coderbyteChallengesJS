245578274c7d9e99a632961f0b4772e6f9c0b478
Gaye Bulut <gayegul@gmail.com>
2015-04-14 12:57:48 -0700

    medium

---
+++ medium/binaryConverter.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//return the decimal form of the binary value. For example:
+//if 101 is passed return 5, or if 1000 is passed return 8.
+
+function BinaryConverter(str) {
+  var result = 0;
+  var powerOfTwo = 0;
+  for(var i = str.length-1; i >= 0; i--) {
+    result += str[i] * Math.pow(2, powerOfTwo);
+    powerOfTwo++;
+  }
+  return result;
+}
