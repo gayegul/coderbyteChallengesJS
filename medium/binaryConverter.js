845dcedb1a28aa170f224d3f1396373a213c4657
Gaye Bulut <gayegul@gmail.com>
2015-04-15 16:26:37 -0700

    refactor

--- medium/binaryConverter.js
+++ medium/binaryConverter.js
@@ -7,7 +7,7 @@ function BinaryConverter(str) {
   var result = 0;
   var powerOfTwo = 0;
   for(var i = str.length-1; i >= 0; i--) {
-    result += str[i] * Math.pow(2, powerOfTwo);
+    result += str[i] === "1" ? Math.pow(2, powerOfTwo) : 0;
     powerOfTwo++;
   }
   return result;
