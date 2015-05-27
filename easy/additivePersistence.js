af2158da5007f9c7dc73412e4c81e4083eb28d0e
Gaye Bulut <gayegul@gmail.com>
2015-05-27 16:14:28 -0700

    fix solution

--- easy/additivePersistence.js
+++ easy/additivePersistence.js
@@ -1,19 +1,24 @@
-'use strict';
+/* For this challenge you will be determining
+the additive persistence for a given number.
+29 -> 2+9=11, 11 -> 1+1=2, answer = 2 times. */
 
-//For this challenge you will be determining 
-//the additive persistence for a given number.
-//29 -> 2+9=11, 11 -> 1+1=2, answer = 2 times
-
-function AdditivePersistence(num) { 
-  var newArr = num.toString().split(""); 
+function AdditivePersistence(num) {
+  if(num < 10) {
+    return 0;
+  }
   var counter = 0;
-  while(newArr.length > 1) {
-    var sum = 0;
-    for (var i = 0; i < newArr.length; i++) {
-      sum += Number(newArr[i]);
-    }
+  while(num >= 10) {
+  	num = addTogether(num);
     counter++;
-    newArr = sum.toString().split("");
   }
   return counter;
+}
+
+function addTogether(num) {
+  var arr = num.toString().split("");
+  var total = 0;
+  for(var i = 0; i < arr.length; i++) {
+    total += Number(arr[i]);
+  }
+  return total;
 }
